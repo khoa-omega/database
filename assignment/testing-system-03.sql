de42d85a06f357bc32f73fed3395995b5fb7eb1f
khoa-omega <nvkhoa05@gmail.com>
2022-10-27 07:07:39 +0700

    Update

--- assignment/testing-system-03.sql
+++ assignment/testing-system-03.sql
@@ -12,12 +12,49 @@ SELECT group_name
 FROM `group`
 WHERE created_date < '2019-12-20';
 
+-- Câu 7: Lấy ra ID của question có >= 4 câu trả lời
+SELECT question_id, COUNT(answer_id)
+FROM answer
+GROUP BY question_id
+HAVING COUNT(answer_id) >= 4;
+
 -- Câu 8: Lấy ra các mã đề thi có thời gian thi >= 60 phút và được tạo trước ngày 20/12/2019
 SELECT `code`
 FROM exam
 WHERE duration >= 60 AND created_date < '2019-12-20';
 
+-- Câu 9: Lấy ra 5 group được tạo gần đây nhất
+SELECT *
+FROM `group`
+ORDER BY created_date DESC
+LIMIT 5;
+
+-- Câu 10: Đếm số nhân viên thuộc department có id = 2
+SELECT COUNT(account_id)
+FROM `account`
+WHERE department_id = 2;
+
 -- Câu 11: Lấy ra nhân viên có tên bắt đầu bằng chữ "D" và kết thúc bằng chữ "o"
 SELECT *
 FROM `account`
 WHERE full_name LIKE 'D%o';
+
+-- Câu 12: Xóa tất cả các exam được tạo trước ngày 20/12/2019
+DELETE FROM exam
+WHERE created_date < '2019-12-20';
+
+-- Câu 13: Xóa tất cả các question có nội dung bắt đầu bằng từ "câu hỏi"
+DELETE FROM question
+WHERE content LIKE 'câu hỏi %';
+
+-- Câu 14: Update thông tin của account có id = 5 thành tên "Nguyễn Bá Lộc" và
+-- email thành loc.nguyenba@vti.com.vn
+UPDATE `account`
+SET full_name = 'Nguyễn Bá Lộc',
+    email = 'loc.nguyenba@vti.com.vn'
+WHERE account_id = 5;
+
+-- Câu 15: Update account có id = 5 sẽ thuộc group có id = 4
+UPDATE group_account
+SET group_id = 4
+WHERE account_id = 5;
--- lesson/lesson-03.sql
+++ lesson/lesson-03.sql
@@ -27,7 +27,7 @@ SELECT *
 FROM department
 WHERE department_id NOT IN (2, 4, 6);
 
--- Toán tử logic: LIKE, NOT LIKE
+-- Toán tử: LIKE, NOT LIKE
 -- WILDCARD: %, _
 -- %: Ký tự đại diện cho 0 hoặc nhiều ký tự
 -- _: Ký tự đại diện 1 ký tự 
@@ -35,7 +35,7 @@ SELECT *
 FROM department
 WHERE department_name NOT LIKE '%i%';
 
--- Toán tử logic: IS NULL, IS NOT NULL
+-- Toán tử: IS NULL, IS NOT NULL
 -- Sử dụng để truy vấn giá trị NULL
 -- Chú ý: không dùng toán tử ==, != với NULL
 SELECT *
@@ -46,12 +46,69 @@ WHERE department_name IS NOT NULL;
 
 -- Từ khóa DISTINCT: Riêng biệt
 -- Dùng sau từ khóa SELECT
+-- Lấy dữ liệu không bị trùng lặp
 SELECT DISTINCT duration
 FROM exam;
 
 -- Từ khóa ORDER BY: Sắp xếp theo
 -- ASC(ending): Tăng dần
 -- DESC(ending): Giảm dần
+-- Mặc định là tăng dần
 SELECT *
 FROM exam
-ORDER BY duration DESC;
+ORDER BY duration ASC;
+
+-- Các hàm tổng hợp: COUNT, SUM, MIN, MAX, AVG
+-- Dùng trong mệnh đề SELECT, HAVING
+-- Chú ý: Chỉ áp dụng cho các giá trị khác NULL
+-- COUNT: Đếm
+-- COUNT(*): Đếm dòng
+-- COUNT(tên_cột): Đếm những dòng mà tên_cột khác NULL
+-- SUM: Tính tổng
+-- MIN, MAX
+-- AVG: Tính trung bình
+SELECT COUNT(*), SUM(duration), MIN(duration), MAX(duration), AVG(duration)
+FROM exam;
+
+-- Mệnh đề GROUP BY / HAVING
+-- GROUP BY: Gom nhóm
+-- HAVING: Điều kiện cho một nhóm
+-- Chú ý: Các cột được SELECT phải thỏa mãn:
+--      1. Là hàm tổng hợp (COUNT, SUM, MIN, MAX hoặc AVG)
+--      2. Là cột dùng để phân nhóm
+SELECT duration, COUNT(*)
+FROM exam
+GROUP BY duration
+HAVING COUNT(*) > 2;
+
+-- ALIAS: Đặt định danh tạm thời cho cột hoặc bảng
+-- Có hiệu lực trong câu truy vấn
+SELECT duration, COUNT(*) AS 'Số bài thi'
+FROM exam
+GROUP BY duration
+HAVING COUNT(*) > 2;
+
+-- LIMIT: Giới hạn số bản ghi trả về
+SELECT *
+FROM exam
+LIMIT 2;
+
+-- Thêm mới dữ liệu
+-- Thứ tự truyền tham số là quan trọng
+INSERT INTO department (department_name)
+VALUES  ('Google'),
+        ('Oppo');
+
+-- Cập nhật dữ liệu
+UPDATE department
+SET department_name = 'Xiaomi'
+WHERE department_name = 'Google';
+
+-- Xóa dữ liệu
+DELETE FROM department
+WHERE department_name = 'Oppo';
+
+-- TRUNCATE: Xóa toàn bộ dữ liệu trong bảng mà không xóa bảng
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE department;
+SET FOREIGN_KEY_CHECKS = 1;

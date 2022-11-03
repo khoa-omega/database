d2e12524986c17f5f412d322bda3254db29bd5fb
khoa-omega <nvkhoa05@gmail.com>
2022-11-03 08:04:33 +0700

    Update

--- assignment/testing-system-05.sql
+++ assignment/testing-system-05.sql
@@ -6,7 +6,19 @@ WHERE department_id =
     (SELECT department_id
     FROM department
     WHERE department_name = 'Sale');
-    
+
+CREATE OR REPLACE VIEW view_quesiton_01_cte AS
+WITH cte_id_of_sale AS (
+    SELECT department_id
+    FROM department
+    WHERE department_name = 'Sale'
+)
+SELECT *
+FROM `account`
+WHERE department_id =
+    (SELECT department_id
+    FROM cte_id_of_sale);
+
 -- Câu 2: Tạo view có chứa thông tin các account tham gia vào nhiều group nhất
 CREATE OR REPLACE VIEW view_question_02_subquery AS
 SELECT `account`.*, COUNT(group_id) AS total_groups
@@ -17,5 +29,49 @@ HAVING total_groups =
     (SELECT MAX(total_groups)
     FROM
         (SELECT COUNT(group_id) AS total_groups
-        FROM group_account
+        FROM `account`
+        LEFT JOIN group_account USING(account_id)
         GROUP BY account_id) AS total_groups_per_account);
+
+CREATE OR REPLACE VIEW view_question_02_cte AS
+WITH cte_total_groups_per_account AS (
+    SELECT `account`.*, COUNT(group_id) AS total_groups
+    FROM `account`
+    LEFT JOIN group_account USING(account_id)
+    GROUP BY account_id
+)
+SELECT *
+FROM cte_total_groups_per_account
+WHERE total_groups =
+    (SELECT MAX(total_groups)
+    FROM cte_total_groups_per_account);
+
+-- Câu 3: Tạo view có chứa câu hỏi có những content quá dài (content quá 300 từ
+-- được coi là quá dài) và xóa nó đi
+CREATE OR REPLACE VIEW view_question_03_subquery AS
+SELECT *
+FROM question
+WHERE CHAR_LENGTH(content) > 300;
+
+DELETE FROM view_question_03_subquery;
+
+-- Câu 5: Tạo view có chứa tất các các câu hỏi do user họ Nguyễn tạo
+CREATE OR REPLACE VIEW view_question_05_subquery AS
+SELECT *
+FROM question
+WHERE creator_id IN
+    (SELECT account_id
+    FROM `account`
+    WHERE full_name LIKE 'Nguyen %');
+
+CREATE OR REPLACE VIEW view_question_05_cte AS
+WITH cte_account_has_username_start_with_nguyen AS (
+    SELECT account_id
+    FROM `account`
+    WHERE full_name LIKE 'Nguyen %'
+)
+SELECT *
+FROM question
+WHERE creator_id IN
+    (SELECT account_id
+    FROM cte_account_has_username_start_with_nguyen)
--- lesson/lesson-05.sql
+++ lesson/lesson-05.sql
@@ -1,8 +1,10 @@
 -- SUBQUERY: Truy vấn con
 -- Kết quả trả về có thể là một bảng hoặc một giá trị
 -- Có thể dùng trong các mệnh đề SELECT, FROM, WHERE...
+-- Khi dùng trong mệnh đề FROM cần đặt ALIAS cho nó
 -- Toán tử: IN, ALL, ANY, EXISTS
 -- ALL: Trả về TRUE khi thỏa mãn tất cả điều kiện
+-- VD: Tìm boy có id lớn nhất trong bảng boy
 SELECT *
 FROM boy
 WHERE id >= ALL (SELECT id FROM boy);
@@ -14,4 +16,30 @@ SELECT *
 FROM boy
 WHERE NOT EXISTS (SELECT id FROM boy WHERE id = 100);
 
+-- VIEW: Bảng ảo, không chứa dữ liệu
+-- Tạo view: CREATE VIEW <view> AS
+-- Xóa view: DROP VIEW <view>;
+-- Tạo mới hoặc thay thế view: CREATE OR REPLACE VIEW <view> AS
+CREATE VIEW view_all_boys AS
+SELECT *
+FROM boy;
 
+SELECT *
+FROM view_all_boys;
+
+-- CTE: Common Table Expression
+-- Bảng ảo, chứa dữ liệu trong bộ nhớ tạm thời
+-- Giúp giảm số lần phải truy cập vào bộ nhớ ổ cứng => cải thiện tốc độ
+-- Có hiệu lực trong câu truy vấn đó
+-- VD: Tạo view có chứa thông tin các account tham gia vào nhiều group nhất
+WITH cte AS (
+    SELECT `account`.*, COUNT(group_id) AS total_groups
+    FROM `account`
+    LEFT JOIN group_account USING(account_id)
+    GROUP BY account_id
+)
+SELECT *
+FROM cte
+WHERE total_groups =
+    (SELECT MAX(total_groups)
+    FROM cte);

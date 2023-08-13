dca4cc90711af0c72b87825da8a7600d61295c97
khoa-omega <nvkhoa05@gmail.com>
2023-08-13 23:14:47 +0700

    [Update] Lesson 01

--- assignment-01.sql
+++ assignment-01.sql
@@ -2,98 +2,98 @@ DROP DATABASE IF EXISTS assignment_01;
 CREATE DATABASE assignment_01;
 USE assignment_01;
 
--- Tạo bảng departments
-DROP TABLE IF EXISTS departments;
-CREATE TABLE departments (
-    department_id INT,
-    department_name VARCHAR(50)
+-- Tạo bảng department
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+	department_id INT,
+	department_name VARCHAR(50)
 );
 
--- Tạo bảng positions
-DROP TABLE IF EXISTS positions;
-CREATE TABLE positions (
-    position_id INT,
-    position_name ENUM('DEVELOPER', 'TESTER', 'SCRUM_MASTER', 'PROJECT_MANAGER')
+-- Tạo bảng position
+DROP TABLE IF EXISTS position;
+CREATE TABLE position (
+	position_id INT,
+	position_name VARCHAR(50)
 );
 
--- Tạo bảng accounts
-DROP TABLE IF EXISTS accounts;
-CREATE TABLE accounts (
-    account_id INT,
-    email VARCHAR(50),
-    username VARCHAR(50),
-    full_name VARCHAR(50),
-    department_id INT,
-    position_id INT,
-    created_date DATE
+-- Tạo bảng account
+DROP TABLE IF EXISTS account;
+CREATE TABLE account (
+	account_id INT,
+	email VARCHAR(50),
+	username VARCHAR(50),
+	full_name VARCHAR(50),
+	department_id INT,
+	position_id INT,
+	created_date DATE
 );
 
--- Tạo bảng groups
-DROP TABLE IF EXISTS `groups`;
-CREATE TABLE IF NOT EXISTS `groups` (
-    group_id INT,
-    group_name VARCHAR(50),
-    creator_id INT,
-    created_date DATE
+-- Tạo bảng group
+DROP TABLE IF EXISTS `group`;
+CREATE TABLE `group` (
+	group_id INT,
+	group_name VARCHAR(50),
+	creator_id INT,
+	created_date DATE
 );
 
--- Tạo bảng groups_accounts
-DROP TABLE IF EXISTS groups_accounts;
-CREATE TABLE groups_accounts (
-    group_id INT,
-    account_id INT,
-    joined_date DATE
+-- Tạo bảng group_account
+DROP TABLE IF EXISTS group_account;
+CREATE TABLE group_account (
+group_id INT,
+	account_id INT,
+	joined_date DATE
 );
 
--- Tạo bảng question_types
-DROP TABLE IF EXISTS question_types;
-CREATE TABLE question_types (
-    type_id INT,
-    type_name ENUM('ESSAY', 'MULTIPLE_CHOICE')
+-- Tạo bảng type_question
+DROP TABLE IF EXISTS type_question;
+CREATE TABLE type_question (
+	type_id INT,
+	type_name VARCHAR(50)
 );
 
--- Tạo bảng question_categories
-DROP TABLE IF EXISTS question_categories;
-CREATE TABLE question_categories (
-    category_id INT,
-    category_name VARCHAR(50)
+-- Tạo bảng category_question
+DROP TABLE IF EXISTS category_question;
+CREATE TABLE category_question (
+	category_id INT,
+	category_name VARCHAR(50)
 );
 
--- Tạo bảng questions
-DROP TABLE IF EXISTS questions;
-CREATE TABLE questions (
-    question_id INT,
-    content VARCHAR(50),
-    category_id INT,
-    type_id INT,
-    creator_id INT,
-    created_date DATE
+-- Tạo bảng question
+DROP TABLE IF EXISTS question;
+CREATE TABLE question (
+	question_id INT,
+	content VARCHAR(50),
+	category_id INT,
+	type_id INT,
+	creator_id INT,
+	created_date DATE
 );
 
--- Tạo bảng answers
-DROP TABLE IF EXISTS answers;
-CREATE TABLE answers (
-    answer_id INT,
-    content VARCHAR(50),
-    question_id INT,
-    is_correct BOOLEAN
+-- Tạo bảng answer
+DROP TABLE IF EXISTS answer;
+CREATE TABLE answer (
+	answer_id INT,
+	content VARCHAR(50),
+	question_id INT,
+	is_correct INT
 );
 
--- Tạo bảng exams
-DROP TABLE IF EXISTS exams;
-CREATE TABLE exams (
-    exam_id INT,
-    code CHAR(10),
-    title VARCHAR(50),
-    category_id INT,
-    duration INT,
-    creator_id INT,
-    created_date DATE
+-- Tạo bảng exam
+DROP TABLE IF EXISTS exam;
+CREATE TABLE exam (
+	exam_id INT,
+	code VARCHAR(50),
+	title VARCHAR(50),
+	category_id INT,
+	duration INT,
+	creator_id INT,
+	created_date DATE
 );
 
--- Tạo bảng exams_questions
-DROP TABLE IF EXISTS exams_questions;
-CREATE TABLE exams_questions (
-    exam_id INT,
-    question_id INT
+-- Tạo bảng exam_question
+DROP TABLE IF EXISTS exam_question;
+CREATE TABLE exam_question (
+	exam_id INT,
+	question_id INT
 );
---
+++ lesson-01.sql
@@ -0,0 +1,34 @@
+-- RDBMS: Hệ quản trị cơ sở dữ liệu quan hệ
+-- Database: Cơ sở dữ liệu
+-- Table: Bảng dữ liệu
+-- Field (Column): Trường dữ liệu (cột)
+-- Record (Row): Bản ghi (hàng)
+
+-- Xóa cơ sở dữ liệu (nếu tồn tại)
+DROP DATABASE IF EXISTS lesson_01;
+
+-- Tạo cơ sở dữ liệu
+CREATE DATABASE lesson_01;
+
+-- Chọn cơ sở dữ liệu cần thao tác
+USE lesson_01;
+
+-- Hiển thị danh sách cơ sở dữ liệu
+SHOW DATABASES;
+
+-- DATA TYPE: Kiểu dữ liệu
+-- INT: Số nguyên
+-- VARCHAR(50): Chuỗi (tối đa 50 ký tự)
+-- DATE: Thời gian (yyyy-MM-dd)
+
+-- Xóa bảng (nếu tồn tại)
+DROP TABLE IF EXISTS department;
+
+-- Tạo bảng
+CREATE TABLE department (
+	department_id INT,
+	department_name VARCHAR(50)
+);
+
+-- Hiển thị danh sách bảng
+SHOW TABLES;
